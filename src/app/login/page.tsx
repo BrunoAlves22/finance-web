d88ebe46d391a434d330d84e80c2b61a9e517786
BrunoAlves22 <brunoalves220701@gmail.com>
2024-12-12 13:17:20 -0300

    update login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -15,15 +15,13 @@ export default async function Login() {
   return (
     <div className="grid h-full grid-cols-2">
       <div className="mx-auto flex h-full max-w-[500px] flex-col justify-center gap-2 p-8">
-        <Image
-          src="/logo.svg"
-          alt="Logo da aplicação - Finance"
-          width={173}
-          height={39}
-          className="select-none"
-        />
+        <h1 className="text-xl font-bold text-black dark:text-white">
+          Finance Web
+        </h1>
 
-        <h1 className="mt-2 text-4xl font-bold text-white">Bem Vindo</h1>
+        <h1 className="mt-2 text-4xl font-bold text-black dark:text-white">
+          Bem Vindo
+        </h1>
         <p className="text-muted-foreground">
           A Finance AI é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações, e oferecer insights personalizados,
@@ -31,9 +29,9 @@ export default async function Login() {
         </p>
 
         <SignInButton>
-          <Button className="mt-4 bg-zinc-800 transition-colors hover:bg-zinc-800/70">
-            <LogInIcon className="mr-2 h-8 w-8" />
-            <span className="text-sm font-light">
+          <Button className="mt-4 bg-zinc-300 transition-colors hover:bg-zinc-300/70 dark:bg-zinc-800 dark:hover:bg-zinc-800/70">
+            <LogInIcon className="mr-2 h-8 w-8 text-black dark:text-white" />
+            <span className="text-sm font-light text-black dark:text-white">
               Fazer login ou Criar conta.
             </span>
           </Button>
